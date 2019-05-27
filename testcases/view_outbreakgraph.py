23bb59171ee609406ae7e496cc3e18604d84b89f
ummezafiirah <40118799+ummezafiirah@users.noreply.github.com>
2019-05-27 14:24:15 +0400

    Update view_outbreakgraph.py

--- testcases/view_outbreakgraph.py
+++ testcases/view_outbreakgraph.py
@@ -10,7 +10,9 @@ URL = "https://mauri-health.herokuapp.com/"
 DISEASE_MENU = "//a[@id='navbarDropdown']"
 DETECT = "//a[@href='/detect/']"
 FLU_GRAPH = "//img[@src='/static/img/flu.png']"
-
+GASTRO_GRAPH = "//img[@src='/static/img/gastro.png']"
+CONJ_GRAPH = "//img[@src='/static/img/conj.png']"
+RESPIRATORY_GRAPH = "//img[@src='/static/img/respiratoryinfection.png']"
 
 #go to link
 go_to_page(URL)
@@ -26,6 +28,13 @@ time.sleep(3)
 #verify if graphs are displayed
 check_element_present(FLU_GRAPH)
 time.sleep(1)
+check_element_present(GASTRO_GRAPH)
+time.sleep(1)
+check_element_present(CONJ_GRAPH)
+time.sleep(1)
+check_element_present(RESPIRATORY_GRAPH)
+time.sleep(1)
+
 
 #closing browser...
 close_browser()
